f89fd633ca5cd7ffb2317e2d10b5f54fa397f9c2
segovelo <gauchosinpingo@gmail.com>
2022-06-03 15:43:51 +0100

    Changes to pass file name from command line

--- api-cli-tool.py
+++ api-cli-tool.py
@@ -21,12 +21,9 @@ def read(url):
 def preview(data):
    pprint(data)
    
-def save(data):
-   with open('fake_data_4.csv', 'w') as f:       
-       #field_names = ['id', 'author', 'title']
-       field_names = list(data[0].keys())
-       print("\n   field names  ", field_names)
-       writer = csv.DictWriter(f, field_names)
+def save(data, file_name):
+   with open(file_name, 'w') as f:       
+       writer = csv.DictWriter(f, list(data[0].keys()))
 
        writer.writeheader()
        for row in data:
@@ -37,7 +34,7 @@ if __name__ == '__main__':
    parser = ArgumentParser(description='A command line tool for interacting with our API')
    parser.add_argument('-r', '--read', action='store_true', help='Sends a GET request to the product API.')
    parser.add_argument('-p', '--preview', action='store_true', help='Shows us a preview of the data.')
-   parser.add_argument('-s', '--save', action='store_true', help='Saves the response to a CSV file.')
+   parser.add_argument('-s', '--save', action='store', help='Saves the response to a CSV file.')
    parser.add_argument('-u', '--url', action='store', help='URL passed as argument')
 
    args = parser.parse_args()
@@ -49,7 +46,7 @@ if __name__ == '__main__':
    if args.preview:
        preview(read(args.url))
    if args.save:
-       save(read(args.url))
+       save(read(args.url), args.save)
    else:
        print('Use the -h or --help flags for help')
 
